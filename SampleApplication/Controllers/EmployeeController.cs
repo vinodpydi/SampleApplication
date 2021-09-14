b8b1cf67d9d546c24ca185d4e0a7a6272528e3cc
vinodpydi <50870615+vinodpydi@users.noreply.github.com>
2021-09-14 15:08:20 +0530

    Commit the MVC Code - which converts object to JSON
    Commit the MVC Code - which converts object to JSON

---
+++ SampleApplication/Controllers/EmployeeController.cs
@@ -0,0 +1,57 @@
+﻿using SampleApplication.Models;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace SampleApplication.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult EmployeeResult()
+        {
+
+            Employee model = new Employee
+            {
+                FirstName = Request["FirstName"],
+                LastName = Request["LastName"]
+            };
+
+            //In this way also we can serialize and deserialize
+
+
+            ////serialize an Employee object to JSON string
+            //ViewBag.json = JsonConvert.SerializeObject(model);
+
+            ////deserialize a JSON string back to Employee object
+            //Employee emp = JsonConvert.DeserializeObject<Employee>(ViewBag.json);
+
+
+            //return View("Index", model);
+
+            //Another way where we can convert to JSON
+
+            if (!string.IsNullOrEmpty(model.FirstName) && !string.IsNullOrEmpty(model.LastName))
+            {
+                var dataSerialize = new JavaScriptSerializer().Serialize(model);
+                var path = Server.MapPath("~/App_Data/");
+
+                // Write that JSON to txt file,  
+                System.IO.File.WriteAllText(path + "output.json", dataSerialize);
+                TempData["msg"] = "Json file Generated! check the file in your App_Data folder";
+                return View("Index", model);
+            }
+            else
+            {
+                TempData["msg"] = "Please enter the FirstName/LastName";
+                return View("Index", model);
+            }
+        }
+    }
+}
---
+++ SampleApplication/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SampleApplication.Models
+{
+    public class Employee
+    {
+        [Display(Name = "FirstName")]
+        [Required(ErrorMessage = "Please Provide Firstname", AllowEmptyStrings = false)]
+        public string FirstName { get; set; }
+
+        [Display(Name = "LastName")]
+        [Required(ErrorMessage = "Please Provide Lastname", AllowEmptyStrings = false)]
+        public string LastName { get; set; }
+    }
+}
